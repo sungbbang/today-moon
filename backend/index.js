d99de876df786aec9eccc564f2a5d58145200688
sungbbang <sinsa5133@gmail.com>
2025-10-04 22:18:24 +0900

    chore: cors 설정

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3000;
+
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+  })
+);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
