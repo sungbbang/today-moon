12bf4aeffc6359259c8b0504e289d621733e3e3b
sungbbang <sinsa5133@gmail.com>
2025-10-04 18:37:09 +0900

    feat: App 컴포넌트 구조 설계

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Header from './components/Header';
+import IntroScreen from './components/screens/IntroScreen';
+import MainScreen from './components/screens/MainScreen';
 
 function App() {
+  const [currentView, setCurrentView] = useState<'intro' | 'main'>('intro');
+
   return (
-    <div>
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+    <div className='bg-black text-white'>
+      <div className='min-h-screen w-screen'>
+        <Header />
+        <main className='mt-8 flex flex-col items-center justify-center px-8'>
+          {currentView === 'intro' ? <IntroScreen /> : <MainScreen />}
+        </main>
+      </div>
     </div>
   );
 }
---
+++ frontend/src/components/screens/IntroScreen.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function IntroScreen() {
+  return <div>IntroScreen</div>;
+}
+
+export default IntroScreen;
