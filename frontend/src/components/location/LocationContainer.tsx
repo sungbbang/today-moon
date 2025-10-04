5ff24b54f6dc64e2a9ff6025831bb480035e1cf8
sungbbang <sinsa5133@gmail.com>
2025-10-04 21:51:11 +0900

    feat: LocationContainer 구조 설계

--- frontend/src/components/location/LocationContainer.tsx
+++ frontend/src/components/location/LocationContainer.tsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { IoLocationOutline } from 'react-icons/io5';
+import LocationModal from './LocationModal';
+import LocationSearchBox from './LocationSearchBox';
 
 function LocationContainer() {
-  return <div>LocationContainer</div>;
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+
+  return (
+    <div className='flex w-full max-w-md items-center justify-between gap-4'>
+      <h3 className='text-xl md:text-2xl'>서울</h3>
+
+      <button
+        onClick={() => setIsModalOpen(true)}
+        className='hidden cursor-pointer hover:underline hover:underline-offset-4 sm:block md:text-lg'
+      >
+        지역 선택
+      </button>
+
+      <button
+        onClick={() => setIsModalOpen(true)}
+        className='cursor-pointer rounded bg-gray-700 p-2 hover:bg-gray-600 sm:hidden'
+      >
+        <IoLocationOutline size={20} />
+      </button>
+
+      {isModalOpen && (
+        <LocationModal onClose={() => setIsModalOpen(false)}>
+          <LocationSearchBox onClose={() => setIsModalOpen(false)} />
+        </LocationModal>
+      )}
+    </div>
+  );
 }
 
 export default LocationContainer;
