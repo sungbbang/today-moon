ade19f9976f933bb4bcaeef2d6e3c19f8bf70e1e
sungbbang <sinsa5133@gmail.com>
2025-10-07 20:17:59 +0900

    style: 달 위상 텍스트 스타일 수정

--- frontend/src/components/moon/MoonPhaseName.tsx
+++ frontend/src/components/moon/MoonPhaseName.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 
 function MoonPhaseName({ nameKo, nameEn }: { nameKo: string; nameEn: string }) {
   return (
-    <div className='text-center tracking-wide text-white capitalize'>
-      <span className='text-lg font-semibold md:text-xl'>{nameKo}</span> |{' '}
-      <span className='text-base md:text-lg'>{nameEn}</span>
+    <div className='space-x-2 text-center tracking-wide text-white capitalize'>
+      <span className='font-semibold sm:text-xl'>{nameKo}</span>
+      <span className='sm:text-xl'>|</span>
+      <span className='text-sm sm:text-base'>{nameEn}</span>
     </div>
   );
 }
