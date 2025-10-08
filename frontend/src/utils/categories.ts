573cbbda819a5df87ba90e8601e5d4f2f29f3790
sungbbang <sinsa5133@gmail.com>
2025-10-08 20:18:29 +0900

    feat: 카테고리 데이터 추가

---
+++ frontend/src/utils/categories.ts
@@ -0,0 +1,14 @@
+import { GiTakeMyMoney } from 'react-icons/gi';
+import { IoSchoolOutline, IoBusinessOutline } from 'react-icons/io5';
+import { PiClover } from 'react-icons/pi';
+import { CiMedicalCross } from 'react-icons/ci';
+import { IoIosHeartEmpty } from 'react-icons/io';
+
+export const categories = [
+  { label: '사랑', icon: IoIosHeartEmpty },
+  { label: '건강', icon: CiMedicalCross },
+  { label: '재물', icon: GiTakeMyMoney },
+  { label: '학업', icon: IoSchoolOutline },
+  { label: '직장', icon: IoBusinessOutline },
+  { label: '행운', icon: PiClover },
+];
