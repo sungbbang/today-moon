b03e8761fa2b861c7e3fecf7afd457004309274e
sungbbang <sinsa5133@gmail.com>
2025-10-13 21:14:45 +0900

    feat: MoonContainer에 toast UI 추가

--- frontend/src/components/moon/MoonContainer.tsx
+++ frontend/src/components/moon/MoonContainer.tsx
@@ -2,6 +2,9 @@ import MoonImage from './MoonImage';
 import { getMoonTimes } from '../../utils/moon';
 import MoonInfo from './MoonInfo';
 import korLunar from 'kor-lunar';
+import { useEffect } from 'react';
+import toast, { Toaster } from 'react-hot-toast';
+import { MdSwipe } from 'react-icons/md';
 
 function MoonContainer({
   date,
@@ -21,13 +24,30 @@ function MoonContainer({
   const { lat, lon } = coordinate;
   const { set, rise } = getMoonTimes(date, lat, lon);
 
+  useEffect(() => {
+    const id = toast(
+      <div className='flex items-center justify-center gap-1 rounded-full py-1 text-xs sm:text-lg'>
+        <span>
+          달을 좌우로 <b>스와이프</b>해보세요
+        </span>
+        <MdSwipe size={22} />
+      </div>,
+      {
+        duration: 2000,
+        position: 'bottom-center',
+      },
+    );
+
+    return () => toast.dismiss(id);
+  }, []);
+
   return (
     <div className='flex flex-col items-center gap-y-4'>
       <MoonImage
         imageIdx={imageIdx}
         onSwipe={dir => changeDate(dir === 'left' ? 1 : -1)}
       />
-
+      <Toaster />
       <MoonInfo imageIdx={imageIdx} set={set} rise={rise} />
     </div>
   );
