eb0d4b81a0172b80c6bc1d88b4a124444eb20468
sungbbang <sinsa5133@gmail.com>
2025-10-13 15:12:02 +0900

    fix: 달 이미지 밝기 적용 누락 수정

--- frontend/src/components/moon/MoonImage.tsx
+++ frontend/src/components/moon/MoonImage.tsx
@@ -26,7 +26,7 @@ function MoonImage({
         src={img.src}
         alt='달 사진'
         draggable={false}
-        className='h-full w-full object-cover p-6 transition-transform duration-300 hover:scale-103 hover:rotate-5 sm:p-0'
+        className='h-full w-full object-cover p-6 brightness-125 transition-transform duration-300 hover:scale-103 hover:rotate-5 sm:p-0'
       />
     </div>
   );
